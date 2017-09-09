83e00b50d348f36e433a5971545c8e1a310644a6
yunis <332963965@qq.com>
2017-09-09 23:38:19 +0800

    struct
    struct

--- Project 01 - GoodAsOldPhones/GoodAsOldPhones/GoodAsOldPhones/Product.swift
+++ Project 01 - GoodAsOldPhones/GoodAsOldPhones/GoodAsOldPhones/Product.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
-class Product {
-    var name : String?
-    var cellImageName : String?
-    var fullscreenImageName : String?
+struct Product {
+    var name: String?
+    var cellImageName: String?
+    var fullscreenImageName: String?
     
-    init(name : String,cellImageName:String,fullscreenImageName : String) {
+    init(name: String, cellImageName: String, fullscreenImageName: String) {
         self.name = name
         self.cellImageName = cellImageName
         self.fullscreenImageName = fullscreenImageName
